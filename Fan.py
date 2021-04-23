e36349fcf98a8c8a8411e4eed9ecbd1d35bb975a
RpiPython <32362653+RpiPython@users.noreply.github.com>
2021-04-23 17:19:00 +0200

    Add files via upload

--- Fan.py
+++ Fan.py
@@ -8,33 +8,29 @@ from gpiozero import CPUTemperature
 PinVentilador = 21
 
 # Configuraciones
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(PinVentilador,GPIO.OUT)
+GPIO.setmode(GPIO.BCM)#Defino el tipo de conexion
+GPIO.setup(PinVentilador,GPIO.OUT)# Se trata de una salida
 pwm = GPIO.PWM(PinVentilador, 20) # Pin del Ventilador y frecuencia del PWM en HZ
-cpu = CPUTemperature()
+cpu = CPUTemperature() # Saber la temperatura que tiene el procesador
 
 #Arranque
 dc = 0;
-pwm.start(dc)
+pwm.start(dc)#Comienzo con una porcentaje, en este caso 0%
 
 try:
     while True:# Loop until Ctl C is pressed to stop.
-        cpu.temperature
-        print(cpu.temperature)
         if cpu.temperature > 57: #Si supera 57 grados
             dc = dc + 10
-            if dc > 100:
+            if dc > 100: # No dejo que la señal esceda de 100 %
                 dc = 100
-            pwm.ChangeDutyCycle(dc)
+            pwm.ChangeDutyCycle(dc) # Asigno el porcentaje
         elif cpu.temperature < 54: # Si se reduce menos de 54
             dc = dc -10
-            if dc< 0:
+            if dc< 0: # No dejo que la señal decaiga de 0 %
                 dc = 0
-            pwm.ChangeDutyCycle(0)
-        
+            pwm.ChangeDutyCycle(0) # Asigno el porcentaje
         time.sleep(1)
 except KeyboardInterrupt:
-  print("Ctl C pressed - ending program")
 
 pwm.stop()                         # stop PWM
 GPIO.cleanup()                     # resets GPIO ports used back to input mode
